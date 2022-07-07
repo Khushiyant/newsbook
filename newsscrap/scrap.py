e3aaaedd3def731b01a011af24eaf22700c3d189
Khushiyant <69671407+Khushiyant@users.noreply.github.com>
2022-07-07 23:21:46 +0530

    added feature to scrap hindu editorials

--- newsscrap/scrap.py
+++ newsscrap/scrap.py
@@ -1,23 +1,27 @@
 import json
-from urllib import response
-
+import requests
+import bs4
 from newsdataapi import NewsDataApiClient
 
-from .summarizer import summarize
+from summarizer import summarize
 import yaml
+import os
 
 # API key authorization, Initialize the client with your API key
 # You can pass empty or with request parameters {ex. (country = "us")}
 
-
 class scrap:
-    def __init__(self, apikey):
+    def __init__(self, apikey:str):
         # api = NewsDataApiClient(apikey=apikey)
         # response = api.news_api(q = "trending" , country = "in", language="en")
         response = json.load(open("newsscrap/test.json"))
         self.data = response['results'] if response['status'] == 'success' else None
 
-    def get_data_newsdataapi(self):
+        # Hindu Editorial Data
+        r = requests.get("https://www.thehindu.com/opinion/editorial/")
+        self.soup = bs4.BeautifulSoup(r.text, "html.parser")
+
+    def get_data_newsdataapi(self) -> dict:
         # return self.data
         context = {
             'post_data': []
@@ -30,17 +34,34 @@ class scrap:
                 print(e)
         return context
 
-    def get_hindu_editorials(self):
-        pass
-    
+    def get_hindu_editorials(self) -> dict:
+        links = []
+        context = {
+            'editorials': []
+        }
+
+        for div in self.soup.find_all("div", {"class": "ES2-100x4-text1 hover-icon"}):
+            links.append(div.find("a")['href'])
+
+        for link in links:
+            r = requests.get(link)
+            soup = bs4.BeautifulSoup(r.text, "html.parser")
+            article = soup.find("div", {"class":"article"})
+
+            title = article.find("h1").text
+            desc =  article.find("h2").text
+            content = article.find("div", id=lambda x: x and x.startswith('content-body-')).text
+            context['editorials'].append((title, desc ,content))
+        return context
+
     @staticmethod
     def get_name():
-        return "scrap v0.1.0"
+        return "scrap v0.2.0"
 
     @staticmethod
     def get_description():
-        return "Scrapes the current news according to geoip2 location."
+        return "Scrapes the current news according to geoip2 location and hindu editorials"
 
 
 if __name__ == "__main__":
-    scrap(yaml.full_load("/tmp/config.yaml")['newsdata']['api_key']).get_data()
+    print(scrap(os.getenv("API_KEY")).get_hindu_editorials())
